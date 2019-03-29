229860cda019c1cc6ee266389890f147f8815a95
Magda Bula <magdalena.bula@gmail.com>
2019-03-29 15:54:42 +0100

    add class 'active' to the clicked link

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ function titleClickHandler(event) {
   console.log("Link was clicked!");
   console.log(event);
 
-  /* remove class 'active' from all article links  */
+  /* [DONE] remove class 'active' from all article links  */
 
   const activeLinks = document.querySelectorAll(".titles a.active");
 
@@ -11,9 +11,12 @@ function titleClickHandler(event) {
     activeLink.classList.remove("active");
   }
 
-  /* add class 'active' to the clicked link */
+  /* [DONE] add class 'active' to the clicked link */
+  const clickedElement = this;
+  clickedElement.classList.add("active");
+  console.log("clickedElement (with plus): " + clickedElement);
 
-  /* remove class 'active' from all articles */
+  /* [DONE] remove class 'active' from all articles */
 
   const activeArticles = document.querySelectorAll(".posts article.active");
 
