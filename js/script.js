869f4a51856b833e11e74f5405d789fdfc1c0959
Magda Bula <magdalena.bula@gmail.com>
2019-03-29 17:21:51 +0100

    Add active articles

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@
 function titleClickHandler(event) {
   console.log("Link was clicked!");
   console.log(event);
+  event.preventDefault();
 
   /* [DONE] remove class 'active' from all article links  */
 
@@ -14,7 +15,7 @@ function titleClickHandler(event) {
   /* [DONE] add class 'active' to the clicked link */
   const clickedElement = this;
   clickedElement.classList.add("active");
-  console.log("clickedElement (with plus): " + clickedElement);
+  console.log("clickedElement" + clickedElement);
 
   /* [DONE] remove class 'active' from all articles */
 
@@ -24,11 +25,20 @@ function titleClickHandler(event) {
     activeArticle.classList.remove("active");
   }
 
-  /* get 'href' attribute from the clicked link */
+  /* [DONE] get 'href' attribute from the clicked link */
 
-  /* find the correct article using the selector (value of 'href' attribute) */
+  const articleSelector = clickedElement.getAttribute("href");
+  console.log(articleSelector);
 
-  /* add class 'active' to the correct article */
+  /* [DONE] find the correct article using the selector (value of 'href' attribute) */
+
+  const targetArticle = document.querySelector(articleSelector);
+  console.log(targetArticle);
+
+  /* [DONE] add class 'active' to the correct article */
+
+  targetArticle.classList.add("active");
+  console.log("targetArticle" + targetArticle);
 }
 
 const links = document.querySelectorAll(".titles a");
