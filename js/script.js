78e2af78476dac99c21c17130950429a34e12745
Magda Bula <magdalena.bula@gmail.com>
2019-04-06 21:53:07 +0200

    Add right side list of tags

--- js/script.js
+++ js/script.js
@@ -298,3 +298,61 @@ function addClickListenersToAuthors() {
   /* END LOOP: for each link */
 }
 addClickListenersToAuthors();
+
+//Tags cloud
+const optTagsListSelector = ".tags.list";
+
+function generateTags() {
+  /* [NEW] create a new variable allTags with an empty array */
+  let allTags = [];
+
+  /* find all articles */
+  const articles = document.querySelectorAll(optArticleSelector);
+
+  /* START LOOP: for every article: */
+  for (let article of articles) {
+    /* find tags wrapper */
+    const tagWrapper = article.querySelector(optArticleTagsSelector);
+    console.log(tagWrapper);
+
+    /* make html variable with empty string */
+    let html = "";
+
+    /* get tags from data-tags attribute */
+    const articleTags = article.getAttribute("data-tags");
+    console.log(articleTags);
+
+    /* split tags into array */
+    const articleTagsArray = articleTags.split(" ");
+    console.log(articleTagsArray);
+
+    /* START LOOP: for each tag */
+    for (let tag of articleTagsArray) {
+      /* generate HTML of the link */
+      const linkHTML = '<li><a href="#tag-' + tag + '">' + tag + "</a></li>";
+      console.log(linkHTML);
+
+      /* add generated code to html variable */
+      html = html + linkHTML;
+
+      /* [NEW] check if this link is NOT already in allTags */
+      if (allTags.indexOf(linkHTML) == -1) {
+        /* [NEW] add generated code to allTags array */
+        allTags.push(linkHTML);
+      }
+
+      /* END LOOP: for each tag */
+    }
+
+    /* insert HTML of all the links into the tags wrapper */
+    tagWrapper.innerHTML = html;
+    /* END LOOP: for every article: */
+  }
+
+  /* [NEW] find list of tags in right column */
+  const tagList = document.querySelector(".tags");
+
+  /* [NEW] add html from allTags to tagList */
+  tagList.innerHTML = allTags.join(" ");
+}
+generateTags();
